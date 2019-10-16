eb6217db3779bc1dd25c80cba6ff68c3406f5955
Jan Kassens <jan@kassens.net>
2019-10-16 14:43:44 -0400

    fix typo

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -3,14 +3,16 @@ const fs = require("fs");
 
 const packageJson = require("../package.json");
 
-if (process.argv.length !== 3 || sha.length !== 42) {
+const commitSha = process.argv[2];
+
+if (process.argv.length !== 3 || commitSha.length !== 42) {
   process.stderr.write(
     `Usage:\n  ${process.argv[0]} ${process.argv[1]} COMMIT_SHA\n`
   );
   process.exit(1);
 }
 
-const version = `0.0.0-master.${sha.substr(0, 8)}`;
+const version = `0.0.0-master.${commitSha.substr(0, 8)}`;
 
 process.stdout.write(`Bumping version to '${version}'.\n`);
 
