e116fbbf5e20ccf59df151f0d7ab9359f8af0e96
Jan Kassens <jan@kassens.net>
2019-10-16 14:17:55 -0400

    Use commit hash as version

--- scripts/bump-version.js
+++ scripts/bump-version.js
@@ -3,7 +3,16 @@ const fs = require("fs");
 
 const packageJson = require("../package.json");
 
-const version = `0.0.0-master.${Date.now()}`;
+const sha = process.env.COMMIT_SHA;
+
+if (!sha || sha.length !== 42) {
+  process.stderr.write(
+    "Expected COMMIT_SHA environment variable to be set to the commit sha.\n"
+  );
+  process.exit(1);
+}
+
+const version = `0.0.0-master.${sha.substr(0, 8)}`;
 
 process.stdout.write(`Bumping version to '${version}'.\n`);
 
